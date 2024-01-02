0eb08bf82837afda3562a6ac0152507d7e0ee943
Two-Jay <djeeee1272@gmail.com>
2024-01-02 20:43:35 +0900

    Parser 객체 단계1 구현

--- 00_Encapsulation/ex01/Parser.cpp
+++ 00_Encapsulation/ex01/Parser.cpp
@@ -9,5 +9,17 @@ Parser::Parser(std::string &path) : _path(path) {
 
 std::vector<Vector2> Parser::parse() {
     std::vector<Vector2> ret;
+    // open file by path
+    std::ifstream file(_path);
+    if (!file.is_open()) {
+        throw std::runtime_error("File not found");
+    }
+
+    // read file line by line
+    std::string line;
+    while (std::getline(file, line)) {
+        // parse line
+        std::cout << line << std::endl;
+    }
     return ret;
 }
--- 00_Encapsulation/ex01/main.cpp
+++ 00_Encapsulation/ex01/main.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
-#include "Point.hpp"
 #include "Vector2.hpp"
+#include "Parser.hpp"
 
 int main(int argc, char** argv) {
     (void)argc;
     (void)argv;
-
-    Point a = Point();
-    Point b = Point(42.42, 42.42);
-
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-
-    Vector2 va = Vector2(a);
-    Vector2 vb = Vector2(b);
-
-    std::cout << va << std::endl;
-    std::cout << vb << std::endl;
+    try {
+        std::string path = argv[1];
+        Parser parser = Parser(path);
+        parser.parse();
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
     return 0;
 }
